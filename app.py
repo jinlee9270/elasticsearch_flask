2eecc917101d2ff9becaa7bf232c202dab761741
jinlee <vito9270@naver.com>
2023-09-26 19:22:41 +0900

    fix

--- app.py
+++ app.py
@@ -31,23 +31,24 @@ def search_product_name():
     results = dict()
 
     query_list = request.args.getlist('description_1')
-    # query_list = {"두부": "3장(약300g)",
-    #            "밀가루": "약1컵(140g)",
-    #             "식용유": "3큰술(21g)",
-    #             "달걀": "3개",
-    #             "맛소금": "약간(2g)",
-    #             "후추가루": "약간",
-    #             "식초": "2큰술(16g)",
-    #             "진간장": "1큰술(10g)",
-    #             "굵은고추가루": "약1/3큰술"
-    #           }
+    query_list = {"두부": "3장(약300g)",
+               "밀가루": "약1컵(140g)",
+                "식용유": "3큰술(21g)",
+                "달걀": "3개",
+                "맛소금": "약간(2g)",
+                "후추가루": "약간",
+                "식초": "2큰술(16g)",
+                "진간장": "1큰술(10g)",
+                "굵은고추가루": "약1/3큰술"
+              }
     try:
         for query in query_list:
             # print(query, query_list[query])
             es_query = create_es_query(query)
             search = es.search(index="product_list_v7", query=es_query, size=10)
+            print(query, search)
             hits = search['hits']['hits']
-            results[f'"{query}"'] = hits
+            results[f"{query}"] = hits
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -55,7 +56,7 @@ def search_product_name():
     response = jsonify(results)
     response.headers.add("Access-Control-Allow-Origin", "*")
     print(response.json)
-    return response
+    return response.json
 
 
 if __name__ == '__main__':
