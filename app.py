ebbdede661febeb3919d13162628bf0f693393ab
jinlee <vito9270@naver.com>
2023-09-26 18:39:44 +0900

    mock 제거

--- app.py
+++ app.py
@@ -28,34 +28,29 @@ def create_es_query(query):
 # Set up the search route
 @app.route('/', methods=['GET'])
 def search_product_name():
-    results = []
+    results = dict()
 
     query_list = request.args.getlist('description_1')
-    query_list = {"두부": "3장(약300g)",
-               "밀가루": "약1컵(140g)"
-    #            #  "식용유": "3큰술(21g)",
-    #            #  "달걀": "3개",
-    #            #  "맛소금": "약간(2g)",
-    #            #  "후추가루": "약간",
-    #            #  "식초": "2큰술(16g)",
-    #            #  "진간장": "1큰술(10g)",
-    #            #  "굵은고추가루": "약1/3큰술"
-              }
+
     try:
         for query in query_list:
-            print(query, query_list[query])
+            # print(query, query_list[query])
             es_query = create_es_query(query)
             search = es.search(index="product_list_v7", query=es_query, size=10)
             hits = search['hits']['hits']
 
+            if query not in results:
+                results[query] = []
+
             for hit in hits:
-                results.append(hit)
+                results[query].append(hit)
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
     response = jsonify(results)
     response.headers.add("Access-Control-Allow-Origin", "*")
+    print(results)
     return response
 
 
